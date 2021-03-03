4e63d15acf76d8bd922b3d3cfc70f0ab35eb70c9
Long Hoang <longhdt@outlook.com>
2021-03-03 09:40:28 +0700

    feat: logging to elasticsearch

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -6,6 +6,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Formatting.Elasticsearch;
+using Serilog.Sinks.Elasticsearch;
+using Serilog.Events;
+
 
 namespace APIGateway
 {
@@ -20,7 +25,19 @@ namespace APIGateway
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseSerilog((ctx, config) =>
+                    {
+                        config
+                            .MinimumLevel.Verbose()
+                            .Enrich.FromLogContext()
+                            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("https://elasticsearch-es-http:9200"))
+                            {
+                                AutoRegisterTemplate = true,
+                                ModifyConnectionSettings = x => x.ServerCertificateValidationCallback((o, certificate, arg3, arg4) => { return true; })
+                                                                 .BasicAuthentication("elastic", "9UM40HQ6hy540aup1xZb0q6r"),
+                            });
+
+                    });
                 });
     }
 }
--- APIGateway/Startup.cs
+++ APIGateway/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using APIGateway.Middlewares;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
